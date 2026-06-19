41ed63690df102dcb5a86b8d306a5d41d786c071
davidgarz2005-art <davidgarz2005@gmail.com>
2026-06-18 22:29:02 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
+# main.py
+from fastapi import FastAPI
 
+# Crear la instancia de la aplicación
+app = FastAPI(
+    title="RinconesCol",
+    description="APP WEB donde encontraras turismo y gastronomia ",
+    version="1.0.0",
+)
+
+# Definir un endpoint
+@app.get("/")
+async def root():
+    return {"message": "¡Bienvenido a nuestra web RinconesCol, disfruta tu experiencia en nuestra maravillosa Web!"}
